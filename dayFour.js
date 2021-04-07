a054a887abddf906494da96298981e7504af13d9
cristo_03 <cristo_isc@hotmail.com>
2021-04-06 22:23:06 -0500

    DAY FOUR

---
+++ dayFour.js
@@ -0,0 +1,39 @@
+console.log('--------- Arithmetic Expression // The + Operator  ----------')
+var a = 10,b = 20;
+var x = 100 + 50;
+x = a + b;
+console.log(x, 'SUMA');
+var problem = (100 + 50) * a;
+console.log(problem);
+console.log('--------- Arithmetic Expression // Bitwise Operators ----------')
+
+var problem1 = 5 & 1;
+console.log(problem1);
+
+var problem2 = 5 | 1;
+console.log(problem2);
+
+var problem3 = 5 ^ 1;
+console.log(problem3);
+
+var problem4 = ~5;
+console.log(problem4);
+
+console.log('--------- Relational Expressions // Equality and Inequality Operators  ----------');
+var var1 = 0;
+var var2 = false;
+var result = var1!==var2;
+console.log("Not double Equality operator works: " + result);
+
+
+console.log('--------- Logical Expressions // Logical AND (&&)  ----------');
+var num1 = 3;
+var num2 = -2;
+
+console.log(num1 > 0 && num2 > 0);
+console.log('--------- Logical Expressions // Logical OR (||)  ----------')
+console.log(num1 > 0 || num2 > 0);
+console.log('--------- Logical Expressions // Logical NOT (!)  ----------')
+console.log(num1 > 0 != num2 > 0);
+console.log('--------- Assignment Expressions//Assignment with Operation ----------')
+
